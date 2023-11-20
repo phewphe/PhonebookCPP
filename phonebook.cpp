dbe6486be74a1cab44ea5e85ba8d069c500aeabd
phewphe <143111888+phewphe@users.noreply.github.com>
2023-11-20 22:48:34 +0530

    Add files via upload

--- phonebook.cpp
+++ phonebook.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 #include <string>
+#include <unordered_map>
+#include <vector>
+#include <stack>
 using namespace std;
 
-// Define a Contact structure
 struct Contact {
     string name;
     string phoneNumber;
     string email;
+    string group;
     Contact* prev;
     Contact* next;
 };
@@ -15,41 +18,64 @@ class Phonebook {
 private:
     Contact* head;
     Contact* tail;
+    stack<Contact*> undoStack;
+    stack<Contact*> redoStack;
+    unordered_map<string, Contact*> contactMap;
+    unordered_map<string, vector<Contact*>> groups;
 
 public:
     Phonebook() : head(nullptr), tail(nullptr) {}
 
-    // Function to add a contact to the phonebook
-    void addContact(string name, string phoneNumber, string email) {
-        if( phoneNumber.length() != (10) ){
-            cout<< "Invalid Number ." << " " << "Enter Again" << endl;
-            return ;
+    void addContact(string name, string phoneNumber, string email, string group) {
+        if (phoneNumber.length() != 10) {
+            cout << "Invalid Number. Enter Again" << endl;
+            return;
         }
+
+        if (contactMap.find(phoneNumber) != contactMap.end() || findDuplicateByName(name)) {
+            cout << "Contact already exists. Cannot add duplicate." << endl;
+            return;
+        }
+
         Contact* newContact = new Contact;
         newContact->name = name;
         newContact->phoneNumber = phoneNumber;
         newContact->email = email;
+        newContact->group = group;
         newContact->prev = nullptr;
         newContact->next = nullptr;
 
+        contactMap[phoneNumber] = newContact;
+        groups[group].push_back(newContact);
+
         if (!head) {
-            // If the phonebook is empty, set the new contact as the head
             head = newContact;
             tail = newContact;
         } else {
-            // Otherwise, add the new contact to the end of the list
             tail->next = newContact;
             newContact->prev = tail;
             tail = newContact;
         }
+
+        undoStack.push(head);
     }
 
-    // Function to search for a contact by name
+    bool findDuplicateByName(string name) {
+        Contact* current = head;
+        while (current) {
+            if (current->name == name) {
+                return true;
+            }
+            current = current->next;
+        }
+        return false;
+    }
+
     void searchContact(string name) {
         Contact* current = head;
         while (current) {
             if (current->name == name) {
-                cout << "Name: " << current->name << " | Phone Number: " << current->phoneNumber << "| Email: " << current->name << endl;
+                cout << "Name: " << current->name << " | Phone Number: " << current->phoneNumber << " | Email: " << current->email << endl;
                 return;
             }
             current = current->next;
@@ -57,16 +83,14 @@ public:
         cout << "Contact not found." << endl;
     }
 
-    // Function to display all contacts
     void displayContacts() {
         Contact* current = head;
         while (current) {
-            cout << "Name: " << current->name << " | Phone Number: " << current->phoneNumber <<" | << Email: " << current->email << endl;
+            cout << "Name: " << current->name << " | Phone Number: " << current->phoneNumber << " | Email: " << current->email << endl;
             current = current->next;
         }
     }
 
-    // Function to delete a contact by name
     void deleteContact(string name) {
         Contact* current = head;
         while (current) {
@@ -89,6 +113,39 @@ public:
         }
         cout << "Contact not found." << endl;
     }
+
+    void undo() {
+        if (!undoStack.empty()) {
+            redoStack.push(head);
+            head = undoStack.top();
+            undoStack.pop();
+            cout << "Undo successful." << endl;
+        } else {
+            cout << "Nothing to undo." << endl;
+        }
+    }
+
+    void redo() {
+        if (!redoStack.empty()) {
+            undoStack.push(head);
+            head = redoStack.top();
+            redoStack.pop();
+            cout << "Redo successful." << endl;
+        } else {
+            cout << "Nothing to redo." << endl;
+        }
+    }
+
+    void displayContactsByGroup(string group) {
+        if (groups.find(group) != groups.end()) {
+            cout << "Contacts in Group " << group << ":" << endl;
+            for (auto contact : groups[group]) {
+                cout << "Name: " << contact->name << " | Phone Number: " << contact->phoneNumber << " | Email: " << contact->email << endl;
+            }
+        } else {
+            cout << "No contacts in group " << group << "." << endl;
+        }
+    }
 };
 
 int main() {
@@ -102,13 +159,16 @@ int main() {
         cout << "2. Search for a Contact" << endl;
         cout << "3. Display All Contacts" << endl;
         cout << "4. Delete a Contact" << endl;
-        cout << "5. Exit" << endl;
+        cout << "5. Undo" << endl;
+        cout << "6. Redo" << endl;
+        cout << "7. Display Contacts by Group" << endl;
+        cout << "8. Exit" << endl;
         cout << "Enter your choice: ";
         cin >> choice;
 
         switch (choice) {
             case '1': {
-                string name, phoneNumber, email;
+                string name, phoneNumber, email, group;
                 cout << "Enter name: ";
                 cin.ignore();
                 getline(cin, name);
@@ -116,7 +176,9 @@ int main() {
                 getline(cin, phoneNumber);
                 cout << "Enter email: ";
                 getline(cin, email);
-                phonebook.addContact(name, phoneNumber, email);
+                cout << "Enter group: ";
+                getline(cin, group);
+                phonebook.addContact(name, phoneNumber, email, group);
                 break;
             }
             case '2': {
@@ -139,12 +201,26 @@ int main() {
                 break;
             }
             case '5':
+                phonebook.undo();
+                break;
+            case '6':
+                phonebook.redo();
+                break;
+            case '7': {
+                string group;
+                cout << "Enter group name: ";
+                cin.ignore();
+                getline(cin, group);
+                phonebook.displayContactsByGroup(group);
+                break;
+            }
+            case '8':
                 cout << "Exiting Phonebook." << endl;
                 break;
             default:
                 cout << "Invalid choice. Try again." << endl;
         }
-    } while (choice != '5');
+    } while (choice != '8');
 
     return 0;
 }
